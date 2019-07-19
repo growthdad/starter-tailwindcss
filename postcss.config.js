98e6259b8c89b75ba5136e9e33e302a6863ffa63
dany suktiawan <dany.onemail@gmail.com>
2019-07-19 15:37:37 +0700

    optimize js

--- postcss.config.js
+++ postcss.config.js
@@ -1,17 +1,27 @@
+// calling tailwindcss
 const tailwindcss = require('tailwindcss')
+
+// optimize from unused css
 const purgecss = require('@fullhuman/postcss-purgecss')({
   content: ['./src/**/*.html'],
+  css: ['./src/**/*.css'],
   defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
 })
+
+// minified css
 const cssnano = require('cssnano') ({
   preset: 'default'
 })
+
 const autoprefixer = require('autoprefixer')
+const postcssimport = require('postcss-import')
 
 module.exports = {
   plugins: [
     tailwindcss,
-    ...process.env.NODE_ENV === 'production' ? [purgecss, require('cssnano')] : [],
+    postcssimport,
+    // purgecss,
+    // ...process.env.NODE_ENV === 'production' ? [purgecss, cssnano] : [],
     autoprefixer
   ]
 }
